f8f5a4bf28a3fc2bd79ea089935bbf7a7a77b14f
heiskane <henipe@gmail.com>
2022-10-07 20:07:07 +0300

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+function purge_elements_by_class(class_name) {
+    let consent_banners = document.getElementsByClassName(class_name)
+
+    for (let i = 0; i < consent_banners.length; i++) {
+        consent_banners[i].parentNode.removeChild(consent_banners[i])
+    }
+
+}
+
+function purge_elements_by_id(element_id) {
+    let consent_box = document.getElementById(element_id)
+
+    if (!consent_box) {
+        return
+    }
+
+    consent_box.parentNode.removeChild(consent_box)
+}
+
+const BANNED_CLASSES = ["js-consent-banner"]
+const BANNED_IDS = ["CybotCookiebotDialog"]
+
+for (let i = 0; i < BANNED_CLASSES.length; i++) {
+    purge_elements_by_class(BANNED_CLASSES[i])
+}
+
+for (let i = 0; i < BANNED_IDS.length; i++) {
+    purge_elements_by_id(BANNED_IDS[i])
+}
