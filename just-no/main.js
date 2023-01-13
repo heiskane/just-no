de9d22a1f57b5850d8a47a3df67cbd048dec2f3e
heiskane <henipe@gmail.com>
2023-01-13 21:43:36 +0200

    bye bye twitch cookie banner

--- just-no/main.js
+++ just-no/main.js
@@ -1,3 +1,4 @@
+
 function purge_elements_by_class(class_name) {
     let consent_banners = document.getElementsByClassName(class_name)
 
@@ -17,13 +18,15 @@ function purge_elements_by_id(element_id) {
     consent_box.parentNode.removeChild(consent_box)
 }
 
-const BANNED_CLASSES = ["js-consent-banner"]
+const BANNED_CLASSES = ["js-consent-banner", "consent-banner"]
 const BANNED_IDS = ["CybotCookiebotDialog"]
 
-for (let i = 0; i < BANNED_CLASSES.length; i++) {
-    purge_elements_by_class(BANNED_CLASSES[i])
-}
+window.addEventListener("load", () => {
+    for (let i = 0; i < BANNED_CLASSES.length; i++) {
+        purge_elements_by_class(BANNED_CLASSES[i])
+    }
 
-for (let i = 0; i < BANNED_IDS.length; i++) {
-    purge_elements_by_id(BANNED_IDS[i])
-}
+    for (let i = 0; i < BANNED_IDS.length; i++) {
+        purge_elements_by_id(BANNED_IDS[i])
+    }
+});
